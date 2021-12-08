f4c829f40e392916af9bcdf2aac7065b8f3a267d
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2021-12-08 16:21:55 -0500

    Remove unnecessary Line class

--- day7/part2.py
+++ day7/part2.py
@@ -1,15 +1,5 @@
 import sys
 
-class Line:
-    def __init__(self, x1, y1, x2, y2):
-        self.p1 = (x1, y1)
-        self.p2 = (x2, y2)
-    
-    def __str__(self):
-        return 'p1: {0}, p2: {1}'.format(self.p1, self.p2)
-
-
-
 def readInputFile(filePath):
     with open(filePath, 'r+') as f:
         positions = f.readline().rstrip().split(',')
