272bb2819be4352a7e25f758d174f3e9cfffa900
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-12-13 11:51:20 -0500

    Add graph functions for day 12

--- utils.js
+++ utils.js
@@ -42,6 +42,14 @@ function getAdjacentPositions2D(row, col, width, height) {
     return positions;
 }
 
+const DIR = {
+    none: null,
+    north: 1,
+    east: 2,
+    south: 3,
+    west: 4,
+}
+
 class Pos {
     constructor(row, col) {
         this.row = row;
@@ -56,28 +64,35 @@ class Node {
 }
 
 class Edge {
-    constructor(node1, node2, weight = 0) {
+    constructor(node1, node2, weight = 1, dir = DIR.none) {
         this.node1 = node1;
         this.node2 = node2;
         this.weight = weight;
+        this.dir = dir;
     }
 }
 
 class DGraphNode {
-    constructor(value, edges = []) {
+    constructor(id, value, edges = []) {
+        this.id = id;
         this.value = value;
         this.edges = edges;
     }
 
-    addEdge(weight, dest_node) {
-        this.edges.push(new Edge(this, dest_node, weight));
+    addEdge(weight, dest_node, dir) {
+        this.edges.push(new Edge(this, dest_node, weight, dir));
     }
 }
 
 class DGraph {
-    constructor(root, nodes = []) {
-        this.root = root;
+    constructor(nodes = []) {
         this.nodes = nodes;
+        this.edges = [];
+        this.nodes.forEach(node => {
+            node.edges.forEach(edge => {
+                this.edges.push(edge);
+            });
+        });
     }
 
     addNodes(nodes) {
@@ -88,10 +103,53 @@ class DGraph {
 
     addNode(node) {
         this.nodes.push(node);
+        node.edges.forEach(edge => {
+            this.edges.push(edge);
+        });
     }
 
-    shortestPath(start, end) {
-        // Implementation of Bellman Ford shortest path algorithm
+    // Bellman Ford shortest path algorithm
+    shortestPath(source) {
+        // Initialize graph
+        this.nodes.forEach(node => {
+            node.distance = Infinity;
+            node.predecessor = null;
+            //distances[i] = Infinity;
+            //predecessors[i] = null;
+        });
+
+        source.distance = 0; // source => source distance is 0
+
+        // Relax edges
+        for (let _ = 0; _ < this.nodes.length - 1; _++) {
+            this.edges.forEach(edge => {
+                let u = edge.node1;
+                let v = edge.node2;
+                if (u.distance + edge.weight < v.distance) {
+                    v.distance = u.distance + edge.weight;
+                    v.predecessor = u;
+                }
+            });
+        }
+
+        // Check for negative-weight cycles
+        this.edges.forEach(edge => {
+            let u = edge.node1;
+            let v = edge.node2;
+            if (u.distance + edge.weight < v.distance) {
+                // Find a negative-weight cycle
+                let negative_loop = [v, u];
+                for (let _ = 0; _ < this.nodes.length - 1; _++) {
+                    u = negative_loop[0];
+                    this.edges.forEach(edge => {
+                        if (u.distance + edge.weight < v.distance) {
+                            negative_loop = [v].concat(negative_loop);
+                        }
+                    });
+                    throw "Graph contains a negative-weight cycle"
+                }
+            }
+        });
     }
 }
 
