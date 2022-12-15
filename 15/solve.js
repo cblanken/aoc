23bd1bbd2b935f14b4e6d472bb47c0e932dfa0a7
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-12-15 13:05:18 -0500

    Add solve day 15 part 2 for sample input (sample1.txt)

--- 15/solve.js
+++ 15/solve.js
@@ -101,14 +101,65 @@ function solve1(data) {
 }
 
 function solve2(data) {
+    data = parseData(data);
+    
+    // Get max width and height form data
+    let max_x = data.reduce((acc, sensor) => Math.max(Math.max(acc, sensor.pos.x), sensor.closest_beacon.x), 0) 
+    let max_y = data.reduce((acc, sensor) => Math.max(Math.max(acc, sensor.pos.y), sensor.closest_beacon.y), 0) 
+    let min_x = data.reduce((acc, sensor) => Math.min(Math.min(acc, sensor.pos.x), sensor.closest_beacon.x), 0) 
+    let min_y = data.reduce((acc, sensor) => Math.min(Math.min(acc, sensor.pos.y), sensor.closest_beacon.y), 0) 
+    console.log(`Max x: ${max_x}; Max y: ${max_y}; Min x: ${min_x}; Min y: ${min_y}`);
+
+    // sample.txt
+    let grid = new utils.Grid(20, 20, 0, 0) // sample.txt
+    
+    // input.txt
+    //let grid = new utils.Grid(4000000, 5, -5, -5) // input.txt
+    
+    data.forEach(sensor => {
+        grid.drawChar(sensor.pos, 'S');
+        grid.drawChar(sensor.closest_beacon, 'B');
+    });
+
+    // Mark positions that can't contain a beacon in `row`
+    grid.grid.forEach((row, r) => {
+        row.forEach((col, x) => {
+            x += grid.x_offset;
+            let pos = new utils.Pos(x, r);
+            data.forEach(sensor => {
+                if (sensor.manhattan_distance >= sensor.pos.getManhattanDistance(pos) && grid.getValue(pos) !== 'B') {
+                    grid.drawChar(pos, '#');
+                }
+            });
+        });
+    });
+    
+    grid.print()
+
+    // Find distress beacon
+    let distress_beacon;
+    grid.grid.every((row, r) => {
+        return row.every((col, c) => {
+            distress_beacon = new utils.Pos(c, r);
+            if (grid.getValue(distress_beacon) === '.') {
+                console.log('FOUND DISTRESS BEACON', distress_beacon)
+                return false;
+            }
+
+            return true;
+        });
+    });
+
+    let tuning_frequency = distress_beacon.x * 4000000 + distress_beacon.y;
+    return tuning_frequency;
 }
 
 // Part 1
 readFile(process.argv[2], 1).then(data => {
-    console.log(`Part 1: ${solve1(data)}`);
+    //console.log(`Part 1: ${solve1(data)}`);
 })
 
 // Part 2
 readFile(process.argv[2], 2).then(data => {
-    //console.log(`Part 2: ${solve2(data)}`);
+    console.log(`Part 2: ${solve2(data)}`);
 })
