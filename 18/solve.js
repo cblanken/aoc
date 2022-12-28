87f1648ac0d13e252ecb5cfb3913811192f15af5
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-12-28 11:11:35 -0500

    Day 18 part 2 WIP

--- 18/solve.js
+++ 18/solve.js
@@ -64,17 +64,11 @@ function getCubicNeighbors(matrix, x, y, z) {
     return neighbors;
 }
 
-function addCube(sa, adjacentCubeCnt) {
-    // Returns the surface area after adding new cube
-    return sa ;
-}
-
 /* Solve */
 function solve1(data) {
     data = parseData(data);
     let max_dim = data.reduce((acc, pos) => math.max(...pos, acc), 0);
     let cubes = math.zeros(max_dim, max_dim, max_dim);
-    let surface_areas = math.zeros(max_dim, max_dim, max_dim);
 
     let sa_total = 0;
     data.forEach(pos => {
@@ -88,7 +82,6 @@ function solve1(data) {
             }
         });
 
-        //console.log("Adding", sa)
         sa_total += sa;
         cubes.set([pos[0], pos[1], pos[2]], 1); // mark each cube in matrix
     });
@@ -97,6 +90,46 @@ function solve1(data) {
 }
 
 function solve2(data) {
+    data = parseData(data);
+    let max_dim = data.reduce((acc, pos) => math.max(...pos, acc), 0) + 1;
+    let cubes = math.zeros(max_dim, max_dim, max_dim);
+    let neighbor_matrix = math.zeros(max_dim, max_dim, max_dim);
+    //console.log(data)
+    //console.log(cubes)
+    //console.log(neighbor_matrix)
+    cubes.forEach((value, index, matrix) => {
+        let n = getCubicNeighbors(cubes, index[0], index[1], index[2]);
+        neighbor_matrix.set(index, n)
+    });
+
+
+    let sa_total = 0;
+    data.forEach(pos => {
+        let neighbors = neighbor_matrix.get([pos[0], pos[1], pos[2]]);
+        //let neighbors = getCubicNeighbors(cubes, pos[0], pos[1], pos[2]);
+        let sa = 6;
+        let empty_pockets = [];
+        neighbors.forEach(n => {
+            if (cubes.get([n[0], n[1], n[2]]) === 1) {
+                sa--;
+                sa_total--;
+            }
+        });
+
+        sa_total += sa;
+        cubes.set([pos[0], pos[1], pos[2]], 1); // mark each cube in matrix
+    });
+
+    cubes.forEach((value, index, matrix) => {
+        let neighbors = neighbor_matrix.get(index);
+        if (value === 0 && neighbors.every(x => cubes.get([x[0], x[1], x[2]]) === 1)) {
+            console.log('FOUND SINGLE TRAPPED AIR POCKET', index)
+            sa_total -= 6;
+        }
+        // TODO: account for pockets larger than 1x1
+    });
+
+    return sa_total;
 }
 
 // Part 1
@@ -106,5 +139,5 @@ readFile(process.argv[2], 1).then(data => {
 
 // Part 2
 readFile(process.argv[2], 2).then(data => {
-    //console.log(`Part 2: ${solve2(data)}`);
+    console.log(`Part 2: ${solve2(data)}`);
 })
