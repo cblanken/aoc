a8f09ab873ea1ea76402f7e37af0b90f8df31eec
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-12-18 19:59:31 -0500

    WIP: Day 17 inline astar from petgraph for modification

--- day17/src/part1.rs
+++ day17/src/part1.rs
@@ -1,10 +1,17 @@
 use std::fmt;
+use std::hash::Hash;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
+use std::collections::{BinaryHeap, HashMap};
 
 use aoc_utils::read_file;
 
 use petgraph::graph::{Node, NodeIndex, DiGraph};
-use petgraph::Directed;
-use petgraph::algo::astar;
+use petgraph::visit::{EdgeRef, GraphBase, IntoEdges, Visitable};
+
+mod scored;
+use scored::MinScored;
+// use scored::MinScored;
+use petgraph::algo::{astar, Measure};
 
 
 #[derive(Debug, PartialEq, PartialOrd)]
@@ -132,6 +139,119 @@ fn build_graph(map: &CityMap) -> DiGraph<u32, u32> {
     DiGraph::<u32, u32>::from_edges(edges)
 }
 
+struct PathTracker<G>
+where
+    G: GraphBase,
+    G::NodeId: Eq + Hash,
+{
+    came_from: HashMap<G::NodeId, G::NodeId>,
+}
+
+impl<G> PathTracker<G>
+where
+    G: GraphBase,
+    G::NodeId: Eq + Hash,
+{
+    fn new() -> PathTracker<G> {
+        PathTracker {
+            came_from: HashMap::new(),
+        }
+    }
+
+    fn set_predecessor(&mut self, node: G::NodeId, previous: G::NodeId) {
+        self.came_from.insert(node, previous);
+    }
+
+    fn reconstruct_path_to(&self, last: G::NodeId) -> Vec<G::NodeId> {
+        let mut path = vec![last];
+
+        let mut current = last;
+        while let Some(&previous) = self.came_from.get(&current) {
+            path.push(previous);
+            current = previous;
+        }
+
+        path.reverse();
+
+        path
+    }
+}
+
+pub fn custom_astar<G, F, H, K, IsGoal>(
+    graph: G,
+    start: G::NodeId,
+    mut is_goal: IsGoal,
+    mut edge_cost: F,
+    mut estimate_cost: H,
+) -> Option<(K, Vec<G::NodeId>)>
+where
+    G: IntoEdges + Visitable,
+    IsGoal: FnMut(G::NodeId) -> bool,
+    G::NodeId: Eq + Hash,
+    F: FnMut(G::EdgeRef) -> K,
+    H: FnMut(G::NodeId) -> K,
+    K: Measure + Copy,
+{
+    let mut visit_next = BinaryHeap::new();
+    let mut scores = HashMap::new(); // g-values, cost to reach the node
+    let mut estimate_scores = HashMap::new(); // f-values, cost to reach + estimate cost to goal
+    let mut path_tracker = PathTracker::<G>::new();
+
+    let zero_score = K::default();
+    scores.insert(start, zero_score);
+    visit_next.push(MinScored(estimate_cost(start), start));
+
+    while let Some(MinScored(estimate_score, node)) = visit_next.pop() {
+        if is_goal(node) {
+            let path = path_tracker.reconstruct_path_to(node);
+            let cost = scores[&node];
+            return Some((cost, path));
+        }
+
+        // This lookup can be unwrapped without fear of panic since the node was necessarily scored
+        // before adding it to `visit_next`.
+        let node_score = scores[&node];
+
+        match estimate_scores.entry(node) {
+            Occupied(mut entry) => {
+                // If the node has already been visited with an equal or lower score than now, then
+                // we do not need to re-visit it.
+                if *entry.get() <= estimate_score {
+                    continue;
+                }
+                entry.insert(estimate_score);
+            }
+            Vacant(entry) => {
+                entry.insert(estimate_score);
+            }
+        }
+
+        for edge in graph.edges(node) {
+            let next = edge.target();
+            let next_score = node_score + edge_cost(edge);
+
+            match scores.entry(next) {
+                Occupied(mut entry) => {
+                    // No need to add neighbors that we have already reached through a shorter path
+                    // than now.
+                    if *entry.get() <= next_score {
+                        continue;
+                    }
+                    entry.insert(next_score);
+                }
+                Vacant(entry) => {
+                    entry.insert(next_score);
+                }
+            }
+
+            path_tracker.set_predecessor(next, node);
+            let next_estimate_score = next_score + estimate_cost(next);
+            visit_next.push(MinScored(next_estimate_score, next));
+        }
+    }
+
+    None
+}
 
 pub fn solve(filepath: &str) -> String {
     let data = read_file(filepath);
@@ -152,11 +272,17 @@ pub fn solve(filepath: &str) -> String {
     // dbg!(start_id, finish_id);
     let start_node = g.node_indices().find(|i| *i == NodeIndex::new(start_id as usize)).unwrap();
     let finish_node = g.node_indices().find(|i| *i == NodeIndex::new(finish_id as usize)).unwrap();
-    let path = astar(&g, start_node, |f| f == finish_node, |_e| 1, |_| 0);
+    let path = custom_astar(&g, start_node, |f| f == finish_node, |_e| 1, |_| 0);
 
     println!("{}", city_map);
     dbg!(&path);
 
+
+    // TODO: include direction tracking and block any paths that
+    // go in the same direction for 3 or more tiles
+
+
+    // TODO: render path on map 
     for ni in path.unwrap().1 {
 
     }
